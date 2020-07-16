14c0e4d812785276070364801e95e6a04b3dc150
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2020-07-16 22:55:19 +1000

    Documenting what needs to be done for path finding on map

--- view/Map.c
+++ view/Map.c
@@ -199,13 +199,20 @@ ConnList MapGetConnections(Map m, PlaceId p)
 }
 
 ////////////////////////////////////////////////////////////////////////
+// PATH FINDING
+// Full lookup if need to recreate each turn will entail 3 lookups for each rail condition
+// and put this together
 
 /**
  * Gets all reachable places in a single move for a player
  * @return
  */
-PlaceId *get_reachable_places_in_move(Map map, Player player, PlaceId currentId, int *places_count) {
+PlaceId *get_reachable_places_in_move(Map map, int distance_by_rail, PlaceId currentId, int *places_count) {
     // For now only return immediate connections no special regard for rail
+
+    // TODO: This function will need to given player and round number determine additional reachable locations
+    // based on number of rail connections that can be made
+
     ConnList connections = MapGetConnections(map, currentId);
 
     *places_count = 0;
