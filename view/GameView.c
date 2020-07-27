bbe0aa3ad95ae42ef48b19773fa3075542366d63
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2020-07-27 21:44:26 +1000

    Fixed GvGetLocationHistory memcpy

--- view/GameView.c
+++ view/GameView.c
@@ -370,7 +370,7 @@ PlaceId *GvGetLocationHistory(GameView gv, Player player,
     // ResolveLocation(GameView gameView, PlayerDetails player, PlaceId unresolvedLocation)
     PlaceId *locationHistory = malloc(sizeof(PlaceId *) * currPlayer->moveCount);
     // copy the moves from the struct onto a dynamically allocated array!
-    memcpy(locationHistory, currPlayer->resolvedMoves, currPlayer->moveCount);
+    memcpy(locationHistory, currPlayer->resolvedMoves, sizeof(PlaceId) * currPlayer->moveCount);
 
     // PlaceId *moves = malloc(sizeof(*moves) * TRAIL_SIZE + 2);
     // moves[0] = SANTANDER;
