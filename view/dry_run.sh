a248f903c098f20619bd178ad38ca9ff4e42baee
z5310814 <z5310814@cse.unsw.edu.au>
2020-07-30 20:59:40 +1000

    submit script doesn't do full submission right now only properly prints all the files to use with give command, dry_run.sh runs the dry run test

---
+++ view/dry_run.sh
@@ -0,0 +1,2 @@
+sh /web/cs2521/20T2/ass/ass2/view/dryrun/dryrun.sh Makefile BinaryHeap.c DraculaView.c GameView.c HashTable.c HunterView.c OurTest.c PathFinding.c Players.c Queue.c Utilities.c testDraculaView.c testGameView.c testHunterView.c testMap.c testUtils.c BinaryHeap.h DraculaView.h GameView.h HashTable.h HunterView.h PathFinding.h Players.h Queue.h Utilities.h testUtils.h
+
